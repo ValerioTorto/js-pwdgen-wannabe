60eb90aec8877ec7607484b2cca5de774f738393
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-09 15:06:09 +0100

    First Bonus

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@
 let name = prompt("Inserisci il tuo nome");
 let surname = prompt("Inserisci il tuo cognome");
 let favcolor = prompt("Inserisci il tuo colore preferito");
+let firstnumber = prompt("Inserisci un numero");
+let secnumber = prompt("Inserisci un altro numero");
+
+// Converto le variabili numero in un NUMERO
+firstnumber = parseInt(firstnumber);
+secnumber = parseInt(secnumber)
+
+// Elaborazione delle statistiche
+let primodivsecondo = firstnumber / secnumber;
 
 // Compliazione del biglietto
 let passgenerator = `
 -------------- Il tuo generatore di password --------------
 > Informazioni sull'utente:
-Nome, Cognome e colore preferito: ${name + surname + favcolor + 21}
+Nome, Cognome e colore preferito: ${name + surname + favcolor + primodivsecondo}
 -----------------------------------------------------------
 `;
 
